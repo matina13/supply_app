c8a7e4c79e7dd312f080ce66294dc6c183b0b563
mirmessier63 <jhn435897@gmail.com>
2025-06-04 15:24:59 +0300

    Update

---
+++ src/main/java/org/example/demo/structs/SupplierMaterialInfo.java
@@ -0,0 +1,31 @@
+package org.example.demo.structs;
+
+public class SupplierMaterialInfo {
+    private int supplier_id;
+    private int material_id;
+    private int price;
+    private int quantity;
+
+    public SupplierMaterialInfo(int supplier_id, int material_id, int price, int quantity) {
+        this.supplier_id = supplier_id;
+        this.material_id = material_id;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public int getSupplier_id() {
+        return this.supplier_id;
+    }
+
+    public int getMaterial_id() {
+        return this.material_id;
+    }
+
+    public int getPrice() {
+        return this.price;
+    }
+
+    public int getQuantity() {
+        return this.quantity;
+    }
+}
